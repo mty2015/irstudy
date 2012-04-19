b0a6451ca64b8bef6a9e4d5e3621f8318624c6bd
lihongfu <tony.li.fly@gmail.com>
2012-04-19 23:02:32 +0800

    ok

--- src/src/cnblogs/jcli/irstudy/suport/struct/BTreeIndexManager.java
+++ src/src/cnblogs/jcli/irstudy/suport/struct/BTreeIndexManager.java
@@ -1,7 +1,14 @@
 package cnblogs.jcli.irstudy.suport.struct;
 
+import java.io.ByteArrayInputStream;
+import java.io.ByteArrayOutputStream;
+import java.io.DataInput;
+import java.io.DataInputStream;
+import java.io.DataOutput;
+import java.io.DataOutputStream;
 import java.io.File;
 import java.io.FileNotFoundException;
+import java.io.IOException;
 import java.io.RandomAccessFile;
 import java.io.Serializable;
 
@@ -37,18 +44,93 @@ public class BTreeIndexManager {
 	public void compress(){
 		
 	}
-	
+	/**
+	 * <pre>
+	 * ---------------------------------------------
+	 * |count(byte)|term(String) * 3|childs(int) * 4
+	 * ---------------------------------------------
+	 * </pre>
+	 *
+	 * @author tony.li.fly@gmail.com
+	 */
 	static class BTreeNode implements Serializable{
+		public static int NODE_LEN = 30;
+		
 		/**
 		 * serialVersionUID
 		 */
 		private static final long serialVersionUID = 1L;
 		
+		private boolean isFresh; 
+
+		private byte[] byteContent;
+		
+		private int count;
+		
 		private String[] terms = new String[3];
 		
-		private BTreeNode[] childs = new BTreeNode[4];
+		private int[] childs = new int[4];
 		
+		public BTreeNode(){
+			
+		}
 		
+		public BTreeNode(byte[] bytes){
+			super();
+			if(bytes == null || bytes.length > NODE_LEN){
+				throw new BTreeException("bytes must not be null and it's len must be less than " + NODE_LEN);
+			}
+			
+			ByteArrayInputStream bis = new ByteArrayInputStream(bytes);
+			DataInput di = new DataInputStream(bis);
+			
+			try {
+				this.count = di.readInt();
+				if(count < 0 || count > 3)
+					throw new IOException("contains error 'count' byte");
+				for(int i = 0;i < count;i++){
+					terms[i] = di.readUTF();
+				}
+				for(int i = 0;i <= count;i++){
+					childs[i] = di.readInt();
+				}
+				childs[count] = di.readInt();
+				
+				this.byteContent = bytes;
+				isFresh = true;
+			} catch (IOException e) {
+				throw new BTreeException("bytes maybe not follow the principle of the BTreeNode",e);
+			}
+		}
+		
+		public boolean insertTerm(String term){
+			isFresh = false;
+			if(count >= 3)
+				return false;
+			terms[count++] = term;
+			return true;
+		}
+		
+		public byte[] getBytes(){
+			if(isFresh)
+				return byteContent;
+			ByteArrayOutputStream baos = new ByteArrayOutputStream();
+			DataOutput dataOutput = new DataOutputStream(baos);
+			try {
+				dataOutput.writeByte(count);
+				for(int i = 0;i < count;i++){
+					dataOutput.writeUTF(terms[i]);
+				}
+				for(int i = 0;i <= count;i++){
+					dataOutput.writeInt(childs[i]);
+				}
+				byteContent = baos.toByteArray();
+				isFresh = true;
+				return byteContent;
+			} catch (IOException e) {
+				throw new BTreeException(e);
+			} 
+		}
 	}
 	
 	
